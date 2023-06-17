9a367f76b98659ed207a267ae90dc2fc87e24d16
unknown <christianbaosmorales23@gmail.com>
2023-06-16 21:17:06 -0500

    First Commit

---
+++ js/file.js
@@ -0,0 +1,33 @@
+function imgSlider(anything, newText) {
+
+    var paragraph = document.getElementById("change_text");
+
+    document.querySelector('.starbucks').src = anything;
+    
+
+    paragraph.innerHTML = newText;
+
+
+   
+    if (document.querySelector('.starbucks').src === "img/img4.png") {
+        var img = document.querySelector('.starbucks');
+        img.style.maxwidth = '600px';
+    }
+
+    //document.getElementById('texto1').innerHTML = "B"
+}
+
+//Cambiar color de circulo al seleccionar una de las img pequeñas //
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+
+//Recargar la pagina al oprimir click en la opcion HOME //
+
+function reload() {
+    let url = window.location.href;
+    window.alert(url);
+}
+
